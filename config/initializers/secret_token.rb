d77f2a02d5b77a404e9af08f339431fb4cbbe142
Olesia Kalyniuk <k.olesia2305@gmail.com>
2019-11-03 12:39:23 -0800

    Init hello_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloApp::Application.config.secret_token = '1141c00b62c269ae3c95d2deb8ddcd494d5044dc41f16e425a06adb31cf3739f88da00176f07a33efbe38dd28ab295930240e6cb38dbf0086e7ad7c022cf5b55'
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+#HelloApp::Application.routes.draw do
+#end
+
+Rails.application.routes.draw do
+ 
+  # You can have the root of your site routed with "root"
+  root 'application#hello_app'
+ 
+end
